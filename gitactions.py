e0d5d754817930c2d1527676b3d4f50f9e289d9c
chaitanya.vatte77@gmail.com <chaitanyavatte77@gmail.com>
2022-07-05 11:01:45 +0530

    this the latest flask

--- gitactions.py
+++ gitactions.py
@@ -1,10 +1,10 @@
-import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! this the first change through git actoins'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run()
